d83a50edb9c9011e1b5b83e89e90658abbc012b9
angelinmexicoofficial <angelinmexicoofficial@gmail.com>
2026-05-25 23:17:44 -0600

    update package and vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/angelrosario",
+  base: "/developer-portfolio",
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
